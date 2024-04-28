95e9e1c053339062d84f93f3d1ad097a618eb76e
OnescuAlex-Vlad <onescu8779@gmail.com>
2024-04-28 14:54:02 +0300

    Added the functionality to mock task and workflow execution

--- main.go
+++ main.go
@@ -16,6 +16,18 @@ type Workflow struct {
 	Tasks []Task
 }
 
+// executeTask simulates the execution of a task by printing a message
+func executeTask(task Task) {
+	fmt.Printf("Executing task: %s\n", task.Name)
+}
+
+// executeWorkflow executes a workflow by sequentially executing each task
+func executeWorkflow(workflow Workflow) {
+	fmt.Printf("Executing workflow: %s\n", workflow.Name)
+	for _, task := range workflow.Tasks {
+		executeTask(task)
+	}
+}
 
 func main() {
 	var workflow Workflow = Workflow{Name: "Test WF",
@@ -24,5 +36,5 @@ func main() {
 			HTTPRequest: "http://localhost:8080",
 		}}}
 
-	fmt.Println(workflow)
+	executeWorkflow(workflow)
 }
